b7b4e6ce2fb0a3566da2630da393550de4a4898f
ThunderKiwi <donttrytheimpossible@gmail.com>
2016-07-19 14:02:44 -0500

    Update R_packages.R

--- Reference/R_packages.R
+++ Reference/R_packages.R
@@ -1,17 +1,35 @@
-# install packages
-install.packages('ggplot2')
+# Must-Have Packages (i.e. Hadleyverse +)
+install.packages('ggplot2') #plyr reshape2 scales stringi/r included.
 install.packages('dplyr')
 install.packages('tidyr')
 install.packages('lubridate')
-install.packages('survey')
-install.packages('reshape2')
-install.packages('rmarkdown')
-install.packages('xtable')
-install.packages('maps')
 install.packages('ggmap')
+install.packages('ggvis')
+install.packages('haven')
+install.packages('rvest')
+install.packages('httr')
+install.packages('readr')
+install.packages('readxl')
+
+
+# Vizualization / Reporting
+install.packages('sjmisc')
+install.packages('sjPlot')
+install.packages('rmarkdown')
+install.packages('installr') #load installr, run install.pandoc()
+library(installr)
+install.pandoc()
+
+
+# Other
+install.packages('editrules')
+install.packages('validate')
+install.packages('Rcmdr')
+install.packages('pander')
+install.packages('survey')
+install.packages('maps')
 install.packages('devtools')
 install.packages('swirl')
-install.packages('ggvis')
 install.packages('twitteR')
 install.packages('streamR')
 install.packages('ROAuth')
